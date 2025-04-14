abbeed6b4b1fd9bd160c90ca25237987feea5678
arisfarid <faegh69@gmail.com>
2025-04-14 16:33:15 +0330

    better Django admin panel 7

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -106,3 +106,136 @@ LOGGING = {
         },
     },
 }
+JAZZMIN_SETTINGS = {
+    # Titel on browser tab (defaults to current admin site title)
+    "site_title": "پنل مدیریت Bivaset",
+
+    # Title on the login screen (defaults to site_title or admin site title)
+    "site_header": "Bivaset",
+
+    # Title on the brand (logo) link (defaults to site_title or admin site title)
+    "site_brand": "Bivaset",
+
+    # Logo to use for your site, must be present in static files, used for login form too (defaults to none)
+    # "site_logo": "books/img/logo.png",
+
+    # Logo to use for login form in dark themes (defaults to site_logo)
+    # "login_logo_dark": None,
+
+    # CSS classes that are applied to the logo above
+    # "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    # "site_icon": None,
+
+    # Welcome text on the login screen
+    "welcome_sign": "به پنل مدیریت Bivaset خوش آمدید",
+
+    # Copyright on the footer
+    "copyright": "Bivaset Ltd",
+
+    # The model admin to search from the search bar, search bar omitted if excluded
+    # "search_model": "auth.User",
+
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    # "user_avatar": None,
+
+    ############
+    # TOP MENU #
+    ############
+    # Links to put along the top menu
+    # "topmenu_links": [ ... ],
+
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+
+    # Hide these apps when generating side menu e.g (auth)
+    # "hide_apps": [],
+
+    # Hide these models when generating side menu (e.g auth.user)
+    # "hide_models": [],
+
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    # "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+
+    # Custom links to append to app groups, keyed on app name
+    # "custom_links": { ... },
+
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
+
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": None,
+    "custom_js": None,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False, # Set to True to allow easy theme customization
+
+    ###############
+    # Change view #
+    ###############
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    # "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+
+    #################
+    # RTL SUPPORT #
+    #################
+    # REQUIRED: Set this True to enable RTL support for the theme
+    "use_rtl": True,
+}
+
+# Optional: Add UI Customizer related settings if you want to customize colors easily
+# JAZZMIN_UI_TWEAKS = {
+#     "navbar_small_text": False,
+#     "footer_small_text": False,
+#     "body_small_text": False,
+#     "brand_small_text": False,
+#     "brand_colour": False,
+#     "accent": "accent-primary",
+#     "navbar": "navbar-white navbar-light",
+#     "no_navbar_border": False,
+#     "navbar_fixed": False,
+#     "layout_boxed": False,
+#     "footer_fixed": False,
+#     "sidebar_fixed": False,
+#     "sidebar": "sidebar-dark-primary",
+#     "sidebar_nav_small_text": False,
+#     "sidebar_disable_expand": False,
+#     "sidebar_nav_child_indent": False,
+#     "sidebar_nav_compact_style": False,
+#     "sidebar_nav_legacy_style": False,
+#     "sidebar_nav_flat_style": False,
+#     "theme": "default", # Use "flatly", "litera", "pulse" etc. for other flat themes
+#     "dark_mode_theme": None,
+#     "button_classes": {
+#         "primary": "btn-primary",
+#         "secondary": "btn-secondary",
+#         "info": "btn-info",
+#         "warning": "btn-warning",
+#         "danger": "btn-danger",
+#         "success": "btn-success"
+#     }
+# }
