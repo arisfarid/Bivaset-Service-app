98019a25ebe9f27f4b0468f0324d97646217f539
arisfarid <faegh69@gmail.com>
2025-04-14 16:21:17 +0330

    better Django admin panel 6

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -5,7 +5,7 @@ import requests
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-your-secret-key-here'  # بعداً یه کلید امن بذار
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['185.204.171.107', 'service.bivaset.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
